2900016a168d26d898636faf4566633ba834eaee
Emmanuel Adebiyi <adebiyiemmanuel11@gmail.com>
2023-05-05 05:23:48 +0100

    update: changes

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -9,20 +9,22 @@ def minOperations(n):
     main: function that calculates the fewest number of operations
         needed to give a result of exactly n H characters in a file
     args: n: Number of characters to be displayed
-    return: number of min operations
+    return: number of min operations - integer
     """
 
-    if n <= 1:
+    # all outputs should be at least 2 char: (min, Copy All => Paste)
+    if (n < 2):
         return 0
-
-    # initialize the DP table
-    dp = [float('inf')] * (n + 1)
-    dp[1] = 0
-
-    # fill the DP table
-    for i in range(2, n + 1):
-        for j in range(1, i):
-            if i % j == 0:
-                dp[i] = min(dp[i], dp[j] + (i // j))
-
-    return dp[n]
+    output, root = 0, 2
+    while root <= n:
+        # if n evenly divides by root
+        if n % root == 0:
+            # total even-divisions by root = total operations
+            output += root
+            # set n to the remainder
+            n = n / root
+            # reduce root to find remaining smaller vals that evenly-divide n
+            root -= 1
+        # increment root until it evenly-divides n
+        root += 1
+    return output
