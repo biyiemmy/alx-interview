c31835050085c29509da4f274a1c1e37df386cb0
Emmanuel Adebiyi <adebiyiemmanuel11@gmail.com>
2023-05-10 17:23:50 +0100

    update: new code

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,39 +1,45 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 """Script that reads stdin line by line and computes metrics"""
 
 import sys
 
-# Define variables to hold metrics
-total_size = 0
-status_counts = {}
 
-# Define function to print statistics
-
-
-def print_statistics():
-    """ prints information """
-    print(f"File size: {total_size}")
-    for status_code in sorted(status_counts.keys()):
-        print(f"{status_code}: {status_counts[status_code]}")
+def printsts(dic, size):
+    """Prints information"""
+    print("File size: {:d}".format(size))
+    for i in sorted(dic.keys()):
+        if dic[i] != 0:
+            print("{}: {:d}".format(i, dic[i]))
 
 
-# Read input lines from stdin
-for i, line in enumerate(sys.stdin):
-    try:
-        # Parse input line
-        ip, _, _, date, _, request, status, size = line.split()
+sts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+       "404": 0, "405": 0, "500": 0}
 
-        # Compute metrics
-        total_size += int(size)
-        status_counts[status] = status_counts.get(status, 0) + 1
+count = 0
+size = 0
 
-        # Print statistics every 10 lines or at keyboard interruption
-        if (i + 1) % 10 == 0:
-            print_statistics()
-    except ValueError:
-        # Skip invalid input lines
-        continue
+try:
+    for line in sys.stdin:
+        if count != 0 and count % 10 == 0:
+            printsts(sts, size)
 
-# Print final statistics
-print_statistics()
+        stlist = line.split()
+        count += 1
+
+        try:
+            size += int(stlist[-1])
+        except ValueError:
+            pass
+
+        try:
+            if stlist[-2] in sts:
+                sts[stlist[-2]] += 1
+        except IndexError:
+            pass
+    printsts(sts, size)
+
+
+except KeyboardInterrupt:
+    printsts(sts, size)
+    raise
