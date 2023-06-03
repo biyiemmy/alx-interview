1ed961a91db02120ac0b206327edf0da0cff6fa1
Emmanuel Adebiyi <adebiyiemmanuel11@gmail.com>
2023-06-03 15:53:28 +0100

    add: semistandard

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,55 +3,55 @@
 const request = require('request');
 
 // Retrieve movie ID from command line argument
-const movieId = process.argv[2]; 
+const movieId = process.argv[2];
 
 if (!movieId) {
-    console.error('Please provide a movie ID as a command line argument.');
-    process.exit(1);
+  console.error('Please provide a movie ID as a command line argument.');
+  process.exit(1);
 }
 
 // Make request to get movie details
 request.get(`https://swapi-api.alx-tools.com/api/films/${movieId}`, (error, response, body) => {
-    if (error) {
-        console.error('Error making API request:', error);
-        process.exit(1);
-    }
+  if (error) {
+    console.error('Error making API request:', error);
+    process.exit(1);
+  }
 
-    if (response.statusCode !== 200) {
-        console.error('API request failed with status code:', response.statusCode);
-        process.exit(1);
-    }
+  if (response.statusCode !== 200) {
+    console.error('API request failed with status code:', response.statusCode);
+    process.exit(1);
+  }
 
-    const movie = JSON.parse(body);
+  const movie = JSON.parse(body);
 
-    // Fetch each character in the movie
-    const characters = movie.characters;
-    const characterPromises = characters.map((characterUrl) => {
-        return new Promise((resolve, reject) => {
-            request.get(characterUrl, (error, response, body) => {
-                if (error) {
-                    reject(error);
-                }
+  // Fetch each character in the movie
+  const characters = movie.characters;
+  const characterPromises = characters.map((characterUrl) => {
+    return new Promise((resolve, reject) => {
+      request.get(characterUrl, (error, response, body) => {
+        if (error) {
+          reject(error);
+        }
 
-                if (response.statusCode !== 200) {
-                    reject(new Error(`API request failed with status code: ${response.statusCode}`));
-                }
+        if (response.statusCode !== 200) {
+          reject(new Error(`API request failed with status code: ${response.statusCode}`));
+        }
 
-                const character = JSON.parse(body);
-                resolve(character.name);
-            });
-        });
+        const character = JSON.parse(body);
+        resolve(character.name);
+      });
     });
+  });
 
-    // Print characters once all promises are resolved
-    Promise.all(characterPromises)
-        .then((characterNames) => {
-            characterNames.forEach((name) => {
-                console.log(name);
-            });
-        })
-        .catch((error) => {
-            console.error('Error fetching character details:', error);
-            process.exit(1);
-        });
+  // Print characters once all promises are resolved
+  Promise.all(characterPromises)
+    .then((characterNames) => {
+      characterNames.forEach((name) => {
+        console.log(name);
+      });
+    })
+    .catch((error) => {
+      console.error('Error fetching character details:', error);
+      process.exit(1);
+    });
 });
