0720f91d914c06df74da860f407401bb0546b998
Emmanuel Adebiyi <adebiyiemmanuel11@gmail.com>
2023-06-07 21:46:46 +0100

    changes

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -28,3 +28,16 @@ def minOperations(n):
         # increment root until it evenly-divides n
         root += 1
     return output
+
+    # """
+    # Returns the fewest number of operations needed to result in exactly
+    # n H characters in the file.
+    # """
+    # operations = 0
+    # min_operations = 2
+    # while n > 1:
+    #     while n % min_operations == 0:
+    #         operations += min_operations
+    #         n /= min_operations
+    #     min_operations += 1
+    # return operations
